5c117b4bf74b6622d440b9ef16d0bc609c5d7b28
dickyrey <dicky.rey97@gmail.com>
2020-07-28 02:13:38 +0700

    home fix

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-import 'package:newsapp/models/news_model.dart';
 import 'package:provider/provider.dart';
 
 import '../../providers/news_provider.dart';
 import '../../utils/constants.dart';
+import 'widgets/browse_news_home_widget.dart';
 import 'widgets/custom_carousel_widget.dart';
 import 'widgets/program_list_widget.dart';
 import 'widgets/top_news_list_widget.dart';
-import 'package:timeago/timeago.dart' as timeago;
 
 class HomePage extends StatelessWidget {
   @override
@@ -56,94 +55,7 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-              // SizedBox(height: 40.0),
-              FutureBuilder<List<NewsModel>>(
-                  future: newsProv.fetchEnglishLanguage(),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.none:
-                        return Center(child: CircularProgressIndicator());
-                        break;
-                      case ConnectionState.waiting:
-                        return Center(child: CircularProgressIndicator());
-                        break;
-                      case ConnectionState.active:
-                        return Center(child: CircularProgressIndicator());
-                        break;
-                      case ConnectionState.done:
-                        if (snapshot.hasError) {
-                          return Center(child: Text('Something error'));
-                        }
-                        if (!snapshot.hasData) {
-                          return Center(child: Text('No Data'));
-                        }
-                        break;
-
-                      default:
-                    }
-                    return ListView.builder(
-                      itemCount: snapshot.data.length,
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      physics: ScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        var news = snapshot.data[index];
-                        return Card(
-                          margin: EdgeInsets.symmetric(
-                            horizontal: 18.0,
-                            vertical: 8.0,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                          elevation: 8.0,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: <Widget>[
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(12.0),
-                                  child: Image.network(
-                                    news.urlToImage ??
-                                        'https://bitsofco.de/content/images/2018/12/broken-1.png',
-                                    fit: BoxFit.cover,
-                                    width: 120.0,
-                                    height: 100.0,
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Text(
-                                          news.title,
-                                          style: headline3,
-                                          maxLines: 2,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                        SizedBox(height: 12.0),
-                                        Text(
-                                          timeago.format(news.publishedAt),
-                                          style: subtitle2,
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  }),
+              BrowseNewsInHome(newsProv: newsProv),
             ],
           ),
         ),
---
+++ lib/screens/home/widgets/browse_news_home_widget.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import '../../../models/news_model.dart';
+import '../../../providers/news_provider.dart';
+import '../../../utils/constants.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+class BrowseNewsInHome extends StatelessWidget {
+  const BrowseNewsInHome({
+    Key key,
+    @required this.newsProv,
+  }) : super(key: key);
+
+  final NewsProvider newsProv;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<NewsModel>>(
+        future: newsProv.fetchEnglishLanguage(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Center(child: CircularProgressIndicator());
+              break;
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+              break;
+            case ConnectionState.active:
+              return Center(child: CircularProgressIndicator());
+              break;
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return Center(child: Text('Something error'));
+              }
+              if (!snapshot.hasData) {
+                return Center(child: Text('No Data'));
+              }
+              break;
+
+            default:
+          }
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            physics: ScrollPhysics(),
+            itemBuilder: (context, index) {
+              var news = snapshot.data[index];
+              return Card(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 18.0,
+                  vertical: 8.0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                elevation: 8.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12.0),
+                        child: Image.network(
+                          news.urlToImage ??
+                              'https://bitsofco.de/content/images/2018/12/broken-1.png',
+                          fit: BoxFit.cover,
+                          width: 120.0,
+                          height: 100.0,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                news.title,
+                                style: headline3,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SizedBox(height: 12.0),
+                              Text(
+                                timeago.format(news.publishedAt),
+                                style: subtitle2,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        });
+  }
+}
